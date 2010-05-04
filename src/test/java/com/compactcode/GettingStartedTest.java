063342e0bbfa1d6223e415542fe43e8e8edd72a2
compactcode <shanonmcquay@gmail.com>
2010-05-04 16:14:49 +1000

    10 very simple examples to start things off

--- src/test/java/com/compactcode/GettingStartedTest.java
+++ src/test/java/com/compactcode/GettingStartedTest.java
@@ -1,76 +1,87 @@
 package com.compactcode;
 
-import static com.compactcode.FluentFunction.function;
-import static com.compactcode.FluentList.fluent;
-import static com.compactcode.Functions.each;
+import static com.compactcode.FluentList.listOf;
+import static com.compactcode.Functions.min;
 import static com.compactcode.Functions.sum;
-import static com.compactcode.Functions.toPropertyValue;
-import static com.google.common.collect.Lists.newArrayList;
+import static com.compactcode.Functions.toInt;
+import static org.hamcrest.number.OrderingComparisons.greaterThan;
+import static org.hamcrest.number.OrderingComparisons.lessThan;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 
-import com.google.common.base.Function;
-
 public class GettingStartedTest {
-
-	public class Order {
-		private final Integer cost;
-		public Order(Integer cost) {
-			this.cost = cost;
-		}
-		public Integer getCost() {
-			return cost;
-		}
-	}
-	
-	public class Customer {
-		private final List<Order> orders;
-		public Customer(Order... orders) {
-			this.orders = newArrayList(orders);
-		}
-		public List<Order> getOrders() {
-			return orders;
-		}
-	}
-	
-	private FluentList<Customer> customers = fluent(
-		new Customer(new Order(100)),
-		new Customer(new Order(150)),
-		new Customer(new Order(200), new Order(300))
-	);
-	
-	private Function<Customer, List<Order>> toOrders = toPropertyValue("orders");
-	private Function<Order, Integer> toCost = toPropertyValue("cost");
 	
 	@Test
-	public void testCountTotalNumberOfOrders() {
-		Integer expected = 4;
-		Integer actual = customers.expand(toOrders).size();
+	public void sumIntegers() {
+		int expected = 16;
+		int actual   = listOf(6, 2, 3, 5).reduce(sum());
 		assertEquals(expected, actual);
 	}
 	
 	@Test
-	public void testCountTotalOrderCost() {
-		Integer expected = 750;
-		Integer actual = customers.expand(toOrders).map(toCost).reduce(sum());
+	public void findMinInteger() {
+		int expected = 2;
+		int actual   = listOf(6, 2, 3, 5).reduce(min());
 		assertEquals(expected, actual);
 	}
 	
 	@Test
-	public void testCountTotalOrderCostPerCustomer() {
-		List<Integer> expected = newArrayList(100, 150, 500);
-		List<Integer> actual = customers.map(toOrders).map(each(toCost)).map(sum());
+	public void convertStringsToIntegers() {
+		List<Integer> expected = Arrays.asList(2, 1, 3);
+		List<Integer> actual   = listOf("2", "1", "3").map(toInt());
 		assertEquals(expected, actual);
 	}
 	
 	@Test
-	public void testFindBiggestSpender() {
-		Function<Customer, Integer> totalSpent = function(toOrders).map(each(toCost)).map(sum());
-		Customer expected = customers.get(2);
-		Customer actual = customers.sort(totalSpent).last();
+	public void findAllIntegersGreaterThan4() {
+		List<Integer> expected = Arrays.asList(6, 7, 5);
+		List<Integer> actual   = listOf(1, 6, 7, 5, 4).filter(greaterThan(4));
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void findFirstIntegerLessThan6() {
+		Integer expected = 5;
+		Integer actual   = listOf(6, 7, 5, 4).filter(lessThan(6)).first();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void removeAllIntegersGreaterThanFour() {
+		List<Integer> expected = Arrays.asList(1, 4);
+		List<Integer> actual   = listOf(1, 6, 7, 5, 4).reject(greaterThan(4));
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void findTheTwoLargestIntegers() {
+		List<Integer> expected = Arrays.asList(4, 7);
+		List<Integer> actual   = listOf(1, 4, 3, 7).sort().last(2);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void removeNullElements() {
+		List<String> expected = Arrays.asList("a", "a");
+		List<String> actual   = listOf(null, "a", null, "a").compact();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void removeDuplicateElements() {
+		List<String> expected = Arrays.asList("a", "b");
+		List<String> actual   = listOf("a", "a", "b", "b").unique();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void createStringRepresentation() {
+		String expected = "1, 2, 3";
+		String actual   = listOf(1, 2, 3).join(", ");
 		assertEquals(expected, actual);
 	}
 	
